cf6f51dca113d9882f19d994d6cf702da37a9656
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-07-25 14:44:48 +0530

    new

--- middleware/commonData.js
+++ middleware/commonData.js
@@ -3,39 +3,50 @@ const Category = require('../models/category.js');
 const Subcategory = require('../models/subcategory.js');
 
 async function injectCommonData(req, res, next) {
-    try {
+  try {
 
-        const category_data = await Category.find({
-            category_hide: 0,
-            category_delete: 0
-        }).sort('category_sort');
+    const category_data = await Category.find({
+      category_hide: 0,
+      category_delete: 0
+    }).sort('category_sort').lean();
+
+    for (const category of category_data) {
+
+      let categoryId = category._id;
+
+      const subcategories = await Subcategory.find({
+        category_id: categoryId
+      }).sort('subcategory_sort');
+
+      //index = category_data.findIndex(x => x._id ==  categoryId);
+
+      // if(categoryId == '669f33387fce56c0bbb5035a'){
+       /*  console.log("INDEXX"+category_data[index])
+        console.log(typeof category_data[index]) */
+      // }
+      // var categoryObj = category_data[index];
+      // console.log("OBJJ"+categoryObj)
+      // categoryObj.subcategory_data = "njknkjn";
+      // console.log("OBJJ"+categoryObj)
+      //category_data[index].subcategories = subcategories;
+      // category['subcategories'] = subcategories;
+
+    //   const subcategoryCount = subcategories.length;
+       category.subcategories = subcategories;
+     }
+
+    // const subcat_data = await Subcategory.find();
 
 
+    res.locals.category_data = category_data;
 
-        const categoriesWithSubcategories = [];
-        for (const category of category_data) {
-            let categoryId = category._id;
+  } catch (error) {
+    console.error('Error fetching common data:', error);
+    res.locals.category_data = [];
 
-            const subcategories = await Subcategory.find({
-                category_id: categoryId
-            }).sort('subcategory_sort');
+  }
 
-            categoriesWithSubcategories.push({
-                ...category.toObject(),
-                subcategories
-            });
-        }
-
-        category.subcategories = subcategories;
-
-        res.locals.category_data = category_data;
-
-    } catch (error) {
-        console.error('Error fetching common data:', error);
-        res.locals.category_data = [];
-    }
-
-    next(); // Proceed to the next middleware or route handler
+  next();
 }
 
 module.exports = injectCommonData;
